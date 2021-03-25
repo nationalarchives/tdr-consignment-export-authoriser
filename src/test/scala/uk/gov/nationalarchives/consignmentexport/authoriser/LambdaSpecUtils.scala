d61967f18e6f81117ddf381bca3608d70e9de4fe
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-03-25 12:15:27 +0000

    Return kms response based on the request

--- src/test/scala/uk/gov/nationalarchives/consignmentexport/authoriser/LambdaSpecUtils.scala
+++ src/test/scala/uk/gov/nationalarchives/consignmentexport/authoriser/LambdaSpecUtils.scala
@@ -1,7 +1,12 @@
 package uk.gov.nationalarchives.consignmentexport.authoriser
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
 import com.github.tomakehurst.wiremock.client.WireMock.{okJson, post, urlEqualTo}
+import com.github.tomakehurst.wiremock.common.FileSource
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.extension.{Parameters, ResponseDefinitionTransformer}
+import com.github.tomakehurst.wiremock.http.{Request, ResponseDefinition}
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import org.mockito.scalatest.MockitoSugar
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, EitherValues}
@@ -9,12 +14,31 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor2}
 
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
 import scala.io.Source.fromResource
+import io.circe.generic.auto._
+import io.circe.parser.decode
 
 class LambdaSpecUtils extends AnyFlatSpec with BeforeAndAfterEach with BeforeAndAfterAll with TableDrivenPropertyChecks with Matchers with MockitoSugar with EitherValues {
 
   val wiremockGraphqlServer = new WireMockServer(9001)
-  val wiremockKmsServer = new WireMockServer(9002)
+  val wiremockKmsServer = new WireMockServer(new WireMockConfiguration().port(9002).extensions(new ResponseDefinitionTransformer {
+    override def transform(request: Request, responseDefinition: ResponseDefinition, files: FileSource, parameters: Parameters): ResponseDefinition = {
+      case class KMSRequest(CiphertextBlob: String)
+      decode[KMSRequest](request.getBodyAsString) match {
+        case Left(err) => throw err
+        case Right(req) =>
+          val charset = Charset.defaultCharset()
+          val plainText = charset.newDecoder.decode(ByteBuffer.wrap(req.CiphertextBlob.getBytes(charset))).toString
+          ResponseDefinitionBuilder
+            .like(responseDefinition)
+            .withBody(s"""{"Plaintext": "$plainText"}""")
+            .build()
+      }
+    }
+    override def getName: String = ""
+  }))
 
   val graphQlPath = "/graphql"
 
@@ -23,8 +47,7 @@ class LambdaSpecUtils extends AnyFlatSpec with BeforeAndAfterEach with BeforeAnd
   def graphqlGetConsignment(filename: String): StubMapping = wiremockGraphqlServer.stubFor(post(urlEqualTo(graphQlPath))
     .willReturn(okJson(fromResource(s"json/$filename.json").mkString)))
 
-  def stubKmsResponse = wiremockKmsServer.stubFor(post(urlEqualTo("/"))
-    .willReturn(okJson(s"""{"Plaintext": "aHR0cDovL2xvY2FsaG9zdDo5MDAxL2dyYXBocWw="}""")))
+  def stubKmsResponse = wiremockKmsServer.stubFor(post(urlEqualTo("/")))
 
   override def beforeEach(): Unit = {
     wiremockGraphqlServer.resetAll()
