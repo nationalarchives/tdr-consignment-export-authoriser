d699985350eddd41f872e127b9d76c01d5c7a537
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-04-30 08:25:37 +0000

    Update tdr-aws-utils to 0.1.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.27"
+  lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.29"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.11"
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.238"
   lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.29"
