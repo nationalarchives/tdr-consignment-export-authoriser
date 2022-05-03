f2aa6f33e406afff89695f3d155665168faf8149
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-05-03 08:24:36 +0000

    Update tdr-graphql-client to 0.0.30

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.29"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.11"
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.238"
-  lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.29"
+  lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.30"
   lazy val log4cats = "org.typelevel" %% "log4cats-core"    % "2.3.0"
   lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j"   % "2.3.0"
   lazy val mockitoScala = "org.mockito" %% "mockito-scala" % "1.17.5"
