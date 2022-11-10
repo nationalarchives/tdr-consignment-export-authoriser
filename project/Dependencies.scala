495f15bbdab9295f012f9788139bdb9e9064045d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-11-10 08:30:21 +0000

    Update pureconfig, pureconfig-cats-effect to 0.17.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,8 +9,8 @@ object Dependencies {
   lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j"   % "2.5.0"
   lazy val mockitoScala = "org.mockito" %% "mockito-scala" % "1.17.12"
   lazy val mockitoScalaTest = "org.mockito" %% "mockito-scala-scalatest" % "1.17.12"
-  lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1"
-  lazy val pureConfigCatsEffect = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.1"
+  lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.2"
+  lazy val pureConfigCatsEffect = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.2"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
   lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.3"
 }
