3cd75dc743c53c8183c451a34ce58ebbb99f2524
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-05-07 08:26:04 +0000

    Update kms-utils to 0.1.169 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.374"
-  lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % "0.1.168"
+  lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % "0.1.169"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.4"
   lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.156"
   lazy val log4cats = "org.typelevel" %% "log4cats-core"    % "2.7.0"
