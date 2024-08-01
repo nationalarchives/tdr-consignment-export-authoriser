bedcb20e2e439595574fe3e1f4f3002c0da412d8
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-08-01 08:25:29 +0000

    Update tdr-generated-graphql to 0.0.382 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.381"
+  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.382"
   lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % "0.1.200"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.4"
   lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.173"
